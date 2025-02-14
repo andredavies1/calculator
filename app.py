82893d702f65eafbc28663946ed012ddc037d666
andredavies <apdavies.pro@gmail.com>
2025-02-13 22:18:29 -0800

    Render index.html template + addition of bootstrap

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask, redirect, url_for #Import libraries
+from flask import Flask, redirect, url_for, render_template #Import libraries
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world(): #Routes URL
-    return "<p>Hello, World!</p>"
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home")) #Redirect page when accessing URL/admin
+    return render_template("index.html", content= "hello")
 
 if __name__ == "__main__": #Initiate Flask/ run script.
     app.run(debug=True)
+
